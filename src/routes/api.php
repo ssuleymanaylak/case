7ac3426dd67858538c2ab5730f56434747ec4e15
ssuleymanaylak <ssuleymanaylak@hotmail.com>
2025-02-04 10:56:15 +0300

    Fixed 404 error for api/locations/route

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('throttle:10,1')->group(function () {
     Route::post('/locations', [YukatechtestController::class, 'store']);
     Route::get('/locations', [YukatechtestController::class, 'index']);
+    Route::get('/locations/route', [YukatechtestController::class, 'route']);
     Route::get('/locations/{id}', [YukatechtestController::class, 'show']);
     Route::put('/locations/{id}', [YukatechtestController::class, 'update']);
     Route::delete('/locations/{id}', [YukatechtestController::class, 'destroy']);
-    Route::get('/locations/route', [YukatechtestController::class, 'route']);
 });
